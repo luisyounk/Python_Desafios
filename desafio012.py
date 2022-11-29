63cfcaf4615466bf89593a9e596e48cbb1bd3ad7
Luis Alexandre Dias dos Santos <luisyounk@gmail.com>
2022-11-29 14:04:14 -0300

    debug comentário, importação somente de trunc

--- desafio012.py
+++ desafio012.py
@@ -1,6 +1,5 @@
-// Criar um programa que leia um numero Real qualquer pelo teclado e mostre na tela a sua porção inteira
+#Criar um programa que leia um numero Real qualquer pelo teclado e mostre na tela a sua porção inteira
 
-
-import math
+from math import trunc
 num = float(input('Digite um valor: '))
-print(f'O valor digitado foi {num} e a sua porção inteira é {math.trunc(num)}')
+print(f'O valor digitado foi {num} e a sua porção inteira é {trunc(num)}')
