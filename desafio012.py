37f03b04b4b7e97e79b3cb1a6ef1fbaf642fe0f8
Luis Alexandre Dias dos Santos <luisyounk@gmail.com>
2022-11-29 14:11:01 -0300

    add 2 comentario

--- desafio012.py
+++ desafio012.py
@@ -3,3 +3,7 @@
 from math import trunc
 num = float(input('Digite um valor: '))
 print(f'O valor digitado foi {num} e a sua porção inteira é {trunc(num)}')
+
+
+'''Poderia ser feito desse modo também
+print(f'O valor digitado foi {num} e a sua porção inteira é {int(num)}')'''
